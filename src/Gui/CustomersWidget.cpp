1f6930a7a0ef9c4a41b8dc85283e21c05c3e83fd
Mark Petryk <mark@lorimarksolutions.com>
2023-07-21 10:53:48 -0500

    update

--- src/Gui/CustomersWidget.cpp
+++ src/Gui/CustomersWidget.cpp
@@ -12,27 +12,45 @@ CustomersWidget()
 {
   addStyleClass( "CustomersWidget" );
 
+  /*
+  ** Apply a layout so everything will fit in the window
+  **
+  */
   auto lw = setLayout( std::make_unique< Wt::WVBoxLayout >() );
 
+  /*
+  ** Standard TableView for now
+  **
+  */
   auto w = std::make_unique< Wt::WTableView >();
   m_view = w.get();
   lw-> addWidget( std::move( w ), 1 );
 
+  /*
+  ** Setup standard control
+  **
+  */
   view()-> setSelectionBehavior( Wt::SelectionBehavior::Rows );
   view()-> setSelectionMode(     Wt::SelectionMode::Single   );
   view()-> setAlternatingRowColors( true );
   view()-> doubleClicked().connect( this, &GCW::Gui::CustomersWidget::doubleClicked );
 
+  /*
+  ** Prepare a column list
+  **
+  */
   std::vector< Wt::WFormModel::Field > fields =
   {
     GCW::Dbo::Customer::Field::id,
     GCW::Dbo::Customer::Field::name,
-    GCW::Dbo::Customer::Field::addr_name,
-    GCW::Dbo::Customer::Field::addr_addr1,
     GCW::Dbo::Customer::Field::addr_phone,
     GCW::Dbo::Customer::Field::addr_email
   };
 
+  /*
+  ** Create the model and apply it to the view
+  **
+  */
   m_model = std::make_shared< GCW::Eng::CustomersModel >( fields );
   view()-> setModel( model() );
 
